c24e68b32e4001c674be6932f5ef1e1e35d175a5
Jon Sagebrand <jonsagebrand@gmail.com>
2016-03-06 18:13:39 +0100

    Added another example

--- Knob/Knob.ino
+++ Knob/Knob.ino
@@ -1,21 +1,41 @@
-/*
- Controlling a servo position using a potentiometer (variable resistor)
- by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
 
- modified on 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Knob
+
+/*
+   The LCD circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
 */
 
 #include <Servo.h>
+#include <LiquidCrystal.h>
 
 Servo myservo;  // create servo object to control a servo
 
 int potpin = 0;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
 
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Booting...");
+  
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+
+  lcd.clear();
 }
 
 void loop() {
@@ -23,5 +43,9 @@ void loop() {
   val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
   myservo.write(val);                  // sets the servo position according to the scaled value
   delay(15);                           // waits for the servo to get there
+
+  lcd.setCursor(0, 0);
+  // print the number of seconds since reset:
+  lcd.print(val);
 }
 
