bd01fafcafb835d431d9294ddf07f1f604ab2b83
Jon Sagebrand <jonsagebrand@gmail.com>
2016-03-09 18:53:28 +0100

    Auto program 1 now working. Also many changes in LCD display

--- ardHoneyServo/ardHoneyServo.ino
+++ ardHoneyServo/ardHoneyServo.ino
@@ -10,11 +10,14 @@
 
 Servo myservo;  // create servo object to control a servo
 
-// initialize the library with the numbers of the interface pins
+///// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 int potPin = 0;  // analog pin used to connect the potentiometer
 int potVal;    // variable to read the value from the analog pin
+String potString = "";
+int potLength = 0;
+int potPosition = 0;
 int lastPotVal = 0; // stores last value from pot
 int maxAngle = 70; // maximum degrees for the servo in each direction
 String motorSpeedString = "";
@@ -24,12 +27,12 @@ int motorSpeed = 0;
 int servoLow = 0;
 int servoHigh = 0;
 
-// Outputs:
+///// Outputs:
 int spinnerRelay = 1;
 
 int spinning = 0;
 
-// Buttons:
+///// Buttons:
 const int startStopButton = 11;
 const int program1Button = 12;
 const int program2Button = 13;
@@ -42,7 +45,7 @@ int lastStartStopButtonState = 0;
 int lastProgram1ButtonState = 0;
 int lastProgram2ButtonState = 0;
 
-// Man/Auto-switch
+///// Man/Auto-switch
 const int autoSwitch = 8;
 //const int manSwitch = 1;
 
@@ -52,7 +55,7 @@ int autoSwitchState = 0;
 int lastAutoSwitchState = 0;
 // inst lastManSwitchState = 0;
 
-// Left/Right-switch
+///// Left/Right-switch
 const int leftSwitch = 10;
 //const int rightSwitch = 10;
 
@@ -67,30 +70,29 @@ long debounce = 200;   // the debounce time, increase if the output flickers
 int i = 0; // just a counter
 
 int program11 = 0;
-int program11Angle = 25;
-long program11Time = 2000;
+int program11Percent = 25;
+long program11RunTime = 60000;
 long program11StartTime = 0;
 int program12 = 0;
-int program12Angle = 50;
-long program12Time = 2000;
+int program12Percent = 50;
+long program12RunTime = 120000;
 long program12StartTime = 0;
 
 int program21 = 0;
-int program21Angle = 50;
-long program21Time = 2000;
+int program21Percent = 50;
+long program21RunTime = 2000;
 int program22 = 0;
-int program22Angle = 75;
-long program22Time = 2000;
+int program22Percent = 75;
+long program22RunTime = 2000;
+
+int displayTime = 0;
+String timeString = "";
+int timeLength = 0;
 
 void setup() {
-  //Serial.begin(9600);      // open the serial port at 9600 bps:
-  //Serial.println("Booting...");
-
-  // set up the LCD's number of columns and rows:
+  ///// set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   lcd.print("Booting...");
-
-  //Serial.println("Initializing in/outputs...");
   lcd.setCursor(0, 1);
   lcd.print("Init. in/outputs");
   pinMode(startStopButton, INPUT);
@@ -101,8 +103,6 @@ void setup() {
   pinMode(leftSwitch, INPUT);
   //pinMode(rightSwitch, INPUT);
   pinMode(spinnerRelay, OUTPUT);
-
-  //Serial.println("Attaching servo...");
   lcd.setCursor(0, 1);
   lcd.print("Attaching servo ");
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
@@ -116,10 +116,19 @@ void loop() {
   program2ButtonState = digitalRead(program2Button);
   autoSwitchState = digitalRead(autoSwitch);
   //manSwitchState = digitalRead(manSwitch);
-  leftSwitchState = digitalRead(leftSwitch);
-  //rightSwitchState = digitalRead(rightSwitch);
+  if (spinning == 0) { // Don't change direction during spinning
+    leftSwitchState = digitalRead(leftSwitch);
+    //rightSwitchState = digitalRead(rightSwitch);
+  }
 
-  checkStatusChange();
+  ///// Check if Manual/Auto-switch is switched. Then stop everything
+  if (autoSwitchState != lastAutoSwitchState) {
+    spinning = 0;
+    servoAngle = 0;
+    powerControl();
+    writeLCD();
+    lastAutoSwitchState = autoSwitchState;
+  }
 
   if (autoSwitchState == 0) {
     manualRun();
@@ -130,43 +139,46 @@ void loop() {
 }
 
 void writeLCD() {
+  ///// Manual/Auto
   lcd.setCursor(0, 0);
   if (autoSwitchState == 1) {
-    lcd.print("Auto");
+    lcd.print("Auto  ");
   }
   else {
-    lcd.print("Man.");
+    lcd.print("Manual");
   }
 
-  lcd.setCursor(5, 0);
+  ///// Stopped/Running
+  lcd.setCursor(8, 0);
   if (spinning == 1 && autoSwitchState == 0) {
-    lcd.print("Running");
+    lcd.print("Running ");
   }
   else if (spinning == 1 && program11 == 1) {
-    lcd.print("Pr1-1: ");
+    lcd.print("Prgm 1-1");
   }
   else if (spinning == 1 && program12 == 1) {
-    lcd.print("Pr1-2: ");
+    lcd.print("Prgm 1-2");
   }
   else if (spinning == 1 && program21 == 1) {
-    lcd.print("Pr2-1: ");
+    lcd.print("Prgm 2-1");
   }
   else if (spinning == 1 && program11 == 1) {
-    lcd.print("Pr2-2: ");
+    lcd.print("Prgm 2-2");
   }
-  else if (autoSwitchState == 0 && spinning == 0) {
+  else if (spinning == 0) {
     lcd.print("Stopped ");
   }
   else {
     lcd.print("Error...");
   }
 
+  ///// Direction and speed
   lcd.setCursor(0, 1);
   if (leftSwitchState == 1) {
-    lcd.print("Left ");
+    lcd.print("Left:");
   }
   else {
-    lcd.print("Right");
+    lcd.print("Right:");
   }
 
   if (leftSwitchState == 1) {
@@ -175,35 +187,51 @@ void writeLCD() {
   else {
     motorSpeed = map(servoAngle, 90, 90 + maxAngle, 0, 100);
   }
+
   motorSpeedString = String(motorSpeed);
   motorSpeedLength = motorSpeedString.length();
-  //lcd.setCursor(7, 0);
-  //lcd.print(motorSpeedLength);
-  lcd.setCursor(6, 1);
-  lcd.print(motorSpeed);
-  lcd.setCursor(6 + motorSpeedLength, 1);
-  lcd.print("%");
+  if (leftSwitchState == 1) {
+    lcd.setCursor(5, 1);
+    lcd.print(motorSpeed);
+    lcd.setCursor(5 + motorSpeedLength, 1);
+    lcd.print("%  ");
+  }
+  else {
+    lcd.setCursor(6, 1);
+    lcd.print(motorSpeed);
+    lcd.setCursor(6 + motorSpeedLength, 1);
+    lcd.print("%  ");
+  }
+
+  if (autoSwitchState == 1) {
+    // Time left on each auto sequence
+    timeString = String(displayTime);
+    timeLength = timeString.length();
+    lcd.setCursor(11, 1);
+    lcd.print(displayTime);
+    lcd.setCursor(11 + timeLength, 1);
+    lcd.print("s   ");
+  }
+  else {
+    potPosition = map(potVal, 0, 1023, 0, 100);
+    potString = String(potPosition);
+    potLength = potString.length();
+    lcd.setCursor(11, 1);
+    lcd.print(potPosition);
+    lcd.setCursor(11 + potLength, 1);
+    lcd.print("%  ");
+  }
 }
 
-/*
-  void writeSerial() {
-  char buffer [100];
-  i = sprintf (buffer, "Pot value: %d, Start/Stop Button: %d, Program 1: %d, Program 2: %d, Auto: %d, Left: %d", potVal, startStopButtonState, program1ButtonState, program2ButtonState, autoSwitchState, leftSwitchState);
-  for (int l = 0; l <= i; l++) {
-    Serial.print(buffer[l]);
-  }
-  Serial.println();
-  }
-*/
-
-void checkStatusChange() {
-  if (potVal < lastPotVal - 5 || potVal > lastPotVal + 5 && autoSwitchState == 0) {
-    servoControl();
-    //writeSerial();
+void manualRun() {
+  ////////////////////////////////////////// Checking inputs ///////////////////////////////////////////
+  ///// Potentiometer
+  if (potVal < lastPotVal - 5 || potVal > lastPotVal + 5) {
+    powerControl();
     writeLCD();
     lastPotVal = potVal;
   }
-  
+  ///// Start/Stop-button
   if (startStopButtonState != lastStartStopButtonState) {
     if (startStopButtonState == 1 && lastStartStopButtonState == 0 && millis() - startStopTime > debounce) {
       if (spinning == 1) {
@@ -216,44 +244,16 @@ void checkStatusChange() {
       //program12 = 0;
       powerControl();
     }
-    //writeSerial();
     writeLCD();
     lastStartStopButtonState = startStopButtonState;
     startStopTime = millis();
   }
-  
-  if (program1ButtonState != lastProgram1ButtonState) {
-    if (program1ButtonState == 1 && lastProgram1ButtonState == 0 && autoSwitchState == 1) {
-      program11 = 1;
-      program11StartTime = millis();
-    }
-    //writeSerial();
-    writeLCD();
-    lastProgram1ButtonState = program1ButtonState;
-  }
-  
-  if (program2ButtonState != lastProgram2ButtonState) {
-    //writeSerial();
-    writeLCD();
-    lastProgram2ButtonState = program2ButtonState;
-  }
-  if (autoSwitchState != lastAutoSwitchState) {
-    spinning = 0;
-    servoAngle = 0;
-    powerControl();
-    //writeSerial();
-    writeLCD();
-    lastAutoSwitchState = autoSwitchState;
-  }
+  ///// Left/Right-switch
   if (leftSwitchState != lastLeftSwitchState) {
-    servoControl();
-    //writeSerial();
+    powerControl();
     writeLCD();
     lastLeftSwitchState = leftSwitchState;
   }
-}
-
-void manualRun() {
   if (leftSwitchState == 1) {
     servoLow = 90 - maxAngle;
     servoHigh = 90;
@@ -262,51 +262,103 @@ void manualRun() {
     servoLow = 90 + maxAngle;
     servoHigh = 90;
   }
-  servoControl();
+  powerControl();
 }
 
 void autoRun() {
-  if (program11 == 1) {
-    if (leftSwitchState == 1) {
-      servoAngle = 90 - program11Angle;
+  ////////////////////////////////////////// Checking inputs ///////////////////////////////////////////
+
+  if (program1ButtonState != lastProgram1ButtonState) {
+    if (program1ButtonState == 1 && lastProgram1ButtonState == 0 && millis() - program11StartTime > debounce) {
+      program11 = 1;
+      program11StartTime = millis();
+    }
+    writeLCD();
+    lastProgram1ButtonState = program1ButtonState;
+  }
+  /*
+    if (program2ButtonState != lastProgram2ButtonState) {
+      writeLCD();
+      lastProgram2ButtonState = program2ButtonState;
+    }
+    if (program11 == 1) {
+      if (leftSwitchState == 1) {
+        servoAngle = 90 - program11Percent;
+      }
+      else {
+        servoAngle = 90 + program11Percent;
+      }
+      spinning = 1;
+      writeLCD();
+      powerControl();
+    }
+  */
+  ///// Start/Stop-button
+  if (startStopButtonState != lastStartStopButtonState) {
+    if (startStopButtonState == 1 && lastStartStopButtonState == 0 && millis() - startStopTime > debounce) {
+      if (spinning == 1) {
+        spinning = 0;
+      }
+      program11 = 0;
+      program12 = 0;
+      powerControl();
+    }
+    writeLCD();
+    lastStartStopButtonState = startStopButtonState;
+    startStopTime = millis();
+  }
+
+  ////////////////////////////////////////// Auto sequences ///////////////////////////////////////////
+  ///// Program 1
+  if (program11 == 1) { // Program 1-1 starts
+    spinning = 1; // Rotation starts
+    if (leftSwitchState == 1) { ///// Check direction switch and set servo angle
+      servoAngle = 90 - maxAngle * program11Percent / 100;
     }
     else {
-      servoAngle = 90 + program11Angle;
+      servoAngle = 90 + maxAngle * program11Percent / 100;
     }
-    spinning = 1;
     writeLCD();
     powerControl();
   }
-  if (program11 == 1 && program11Time >= (millis() - program11StartTime)) {
-    if (leftSwitchState == 1) {
-      servoAngle = 90 - program12Angle;
-    }
-    else {
-      servoAngle = 90 + program12Angle;
-    }
-    writeLCD();
-    servoControl();
-    program11 = 0;
+  if (program11 == 1 && millis() - program11StartTime > program11RunTime) { // Program 1-2 starts
+    program11 = 0; // Sequence 1-1 stops
     program12 = 1;
     program12StartTime = millis();
+    if (leftSwitchState == 1) { ///// Check direction switch and set servo angle
+      servoAngle = 90 - maxAngle * program12Percent / 100;
+    }
+    else {
+      servoAngle = 90 + maxAngle * program12Percent / 100;
+    }
+    writeLCD();
+    powerControl();
   }
-  if (program12 == 1 && program12Time >= (millis() - program12StartTime)) {
+
+  if (program12 == 1 && millis() - program12StartTime > program12RunTime) { // Last sequence finished
     servoAngle = 90;
     spinning = 0;
     writeLCD();
     powerControl();
     program12 = 0;
-    program11StartTime = 0;
-    program12StartTime = 0;
   }
+
+  ///// Running time left
+  if (spinning == 1) {
+    if (program11 == 1) {
+      displayTime = (program11RunTime - millis() + program11StartTime) / 1000;
+    }
+    else if (program12 == 1) {
+      displayTime = (program12RunTime - millis() + program12StartTime) / 1000;
+    }
+  }
+  else {
+    displayTime = 0;
+  }
+  writeLCD();
 }
 
 void powerControl() {
-  servoControl();
-  digitalWrite(spinnerRelay, spinning);
-}
-
-void servoControl() {
   if (spinning == 0) {
     servoAngle = 90;
   }
@@ -314,7 +366,8 @@ void servoControl() {
     servoAngle = map(potVal, 1023, 0, servoLow, servoHigh);
   }
 
-  myservo.write(servoAngle);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
+  myservo.write(servoAngle); // sets the servo position according to the scaled value
+  delay(100); // waits for the servo to get there
+  digitalWrite(spinnerRelay, spinning);
 }
 
