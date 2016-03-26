ee6bf2cd0af2bf41f7c72dbe18832c6b80f6d558
Jon Sagebrand <jonsagebrand@gmail.com>
2016-03-27 00:51:45 +0100

    st colon blinks in program mode 1

--- ardHoneyServo/ardHoneyServo.ino
+++ ardHoneyServo/ardHoneyServo.ino
@@ -17,6 +17,7 @@ Servo myservo;  // create servo object to control a servo
 LiquidCrystal595 lcd(5, 6, 7); // 3 pin LCD: datapin, latchpin, clockpin
 // LiquidCrystal lcd(7, 6, 5, 4, 3, 2); // 6 pin LCD
 
+///// manual inputs
 int manualAnglePin = 0;  // analog pin used to connect the potentiometer
 int maxAnglePin = 1; // analog pin used to set max angle
 int zeroOffsetPin = 2; // analog pin used to set zero angle
@@ -30,21 +31,21 @@ int potPosition = 0; // holds position of print to lcd
 int maxAngleValue; // variable to read the value from analog pin
 int zeroOffsetValue; // variable to read the value from analog pin
 
-String motorSpeedString = ""; // holds speed as string
-int motorSpeedLength = 0; // holds number of characters for motorSpeedString
+//String motorSpeedString = ""; // holds speed as string
+//int motorSpeedLength = 0; // holds number of characters for motorSpeedString
 
 int maxAngle; // maximum degrees for the servo in each direction
 int zeroOffset; // value for zero offset of servo
-int servoAngle = 0; 
+int servoAngle = 0;
 int motorSpeed = 0;
 int servoLow = 0;
 int servoHigh = 0;
 
-///// Outputs:
-int spinnerRelay = 4; // pin to set relay
+///// Digital outputs:
+int spinnerRelay = 4; // pin for spinner relay
+bool spinning = 0; // high if spinner is spinning
 
-int spinning = 0;
-
+///// Digital inputs:
 ///// Buttons:
 const int startStopButton = 11;
 const int program1Button = 12;
@@ -61,52 +62,59 @@ int lastProgram2ButtonState = 0;
 ///// Man/Auto-switch
 const int autoSwitch = 8;
 //const int manSwitch = 1;
-
 int autoSwitchState = 0;
 //int manSwitchState = 0;
-
 int lastAutoSwitchState = 0;
 // inst lastManSwitchState = 0;
 
 ///// Left/Right-switch
 const int leftSwitch = 10;
 //const int rightSwitch = 10;
-
 int leftSwitchState = 0;
 //int rightSwitchState = 1;
-
 int lastLeftSwitchState = 0;
 // int lastRightSwitchState = 0;
 
-long startStopTime = 0;         // the last time Start/Stop was toggled
+long startStopTimeMillis = 0; // the last time Start/Stop was toggled
 long debounce = 200;   // the debounce time, increase if the output flickers
-int i = 0; // just a counter
 
-int program11 = 0;
-int program11Percent = 25;
-long program11RunTime = 60; // first sequence run time in seconds
-long program11StartTime = 0;
-int program12 = 0;
-int program12Percent = 50;
-long program12RunTime = 120; // second sequence run time in seconds
-long program12StartTime = 0;
+int program11 = 0; // set when in auto sequence 1-1
+int program11Percent = 25; // initial value, in percent, for servo in auto sequence 1-1
+int program11RunTime = 60; // initial time, in seconds, for auto sequence 1-1
+int program11StartTime = 0;
+int program12 = 0; // set when in auto sequence 1-2
+int program12Percent = 50; // initial value, in percent,  for servo in auto sequence 1-2
+int program12RunTime = 120; // initial time, in seconds, for auto sequence 1-2
+int program12StartTime = 0;
+bool programMode1 = 0;
+int programModeValue = 0; // what of the values are being programmed
+unsigned long blinkColonMillis;
+//bool blinkOut = 0;
+String blinkChar = "=";
 
-int program21 = 0;
-int program21Percent = 50;
-long program21RunTime = 2;
-int program22 = 0;
-int program22Percent = 75;
-long program22RunTime = 2;
+int program21 = 0; // set when in auto sequence 2-1
+int program21Percent = 50; // initial value, in percent,  for servo in auto sequence 2-1
+int program21RunTime = 30; // initial time, in seconds, for auto sequence 2-1
+int program22 = 0; // set when in auto sequence 2-2
+int program22Percent = 75; // initial value, in percent,  for servo in auto sequence 2-2
+int program22RunTime = 150; // initial time in, in seconds, for auto sequence 2-2
+bool programMode2 = 2;
 
 int displayTime = 0;
-String timeString = "";
-int timeLength = 0;
+//String timeString = "";
+//int timeLength = 0;
+
+
+String valString = ""; // for calculation of length of values
+int valLength;
+
+int i = 0; // just a counter
 
 void setup() {
   ///// set up the LCD's number of columns and rows:
   lcd.begin(16, 2);  // define LCD layout
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Booting...");
   delay(500);
   lcd.setCursor(0, 1);
@@ -131,7 +139,7 @@ void loop() {
   manualAngleValue = analogRead(manualAnglePin); // reads the value of the potentiometer (value between 0 and 1023)
   maxAngleValue = analogRead(maxAnglePin);
   zeroOffsetValue = analogRead(zeroOffsetPin);
-  
+
   startStopButtonState = digitalRead(startStopButton);
   program1ButtonState = digitalRead(program1Button);
   program2ButtonState = digitalRead(program2Button);
@@ -155,8 +163,45 @@ void loop() {
   zeroOffset = map(zeroOffsetValue, 1023, 0, -25, 25);
   ///// Set max angle for servo
   maxAngle = map(maxAngleValue, 0, 1023, 0, 90);
-  
-  if (autoSwitchState == 0) {
+
+  if (autoSwitchState == 0 && program1ButtonState == 1 && spinning == 0 && programMode1 != 1) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Entering auto");
+    lcd.setCursor(0, 1);
+    lcd.print("program 1...");
+    programMode1 = 1;
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("1-1%:");
+    lcd.setCursor(8, 0);
+    lcd.print("1-1s:");
+    lcd.setCursor(0, 1);
+    lcd.print("1-2%:");
+    lcd.setCursor(8, 1);
+    lcd.print("1-2s:");
+    writeLCDProgramMode(program11Percent, program11RunTime, program12Percent, program12RunTime);
+    programModeValue = 1;
+    blinkColonMillis = millis();
+  }
+  if (programMode1 == 1 && startStopButtonState == 1) {
+    programMode1 = 0;
+    programModeValue = 0;
+    startStopButtonState = 0;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Exiting program");
+    lcd.setCursor(0, 1);
+    lcd.print("mode...");
+    delay(1000);
+    lcd.clear();
+  }
+
+  if (programMode1 == 1) {
+    programSequence1();
+  }
+  else if (autoSwitchState == 0) {
     manualRun();
   }
   else {
@@ -214,38 +259,83 @@ void writeLCD() {
     motorSpeed = map(servoAngle, 90, 90 + maxAngle, 0, 100);
   }
 
-  motorSpeedString = String(motorSpeed);
-  motorSpeedLength = motorSpeedString.length();
+  valLength = intToStringToLength(motorSpeed);
   if (leftSwitchState == 1) {
     lcd.setCursor(5, 1);
     lcd.print(motorSpeed);
-    lcd.setCursor(5 + motorSpeedLength, 1);
+    lcd.setCursor(5 + valLength, 1);
     lcd.print("%  ");
   }
   else {
     lcd.setCursor(6, 1);
     lcd.print(motorSpeed);
-    lcd.setCursor(6 + motorSpeedLength, 1);
+    lcd.setCursor(6 + valLength, 1);
     lcd.print("%  ");
   }
 
   if (autoSwitchState == 1) {
     // Time left on each auto sequence
-    timeString = String(displayTime);
-    timeLength = timeString.length();
+    valLength = intToStringToLength(displayTime);
     lcd.setCursor(11, 1);
     lcd.print(displayTime);
-    lcd.setCursor(11 + timeLength, 1);
+    lcd.setCursor(11 + valLength, 1);
     lcd.print("s   ");
   }
   else {
     potPosition = map(manualAngleValue, 0, 1023, 0, 100);
-    potString = String(potPosition);
-    potLength = potString.length();
+    valLength = intToStringToLength(potPosition);
     lcd.setCursor(11, 1);
     lcd.print(potPosition);
-    lcd.setCursor(11 + potLength, 1);
+    lcd.setCursor(11 + valLength, 1);
     lcd.print("%  ");
+  }
+}
+
+void writeLCDProgramMode(int firstPercent, int firstRunTime, int secondPercent, int secondRunTime) {
+  ////////////////////////////////////////// print values to LCD ///////////////////////////////////////////
+  lcd.setCursor(5, 0);
+  lcd.print(firstPercent);
+  valLength = intToStringToLength(firstPercent);
+  if (valLength == 1) {
+    lcd.setCursor(5 + valLength, 0);
+    lcd.print("  ");
+  }
+  else if (valLength == 2) {
+    lcd.setCursor(5 + valLength, 0);
+    lcd.print(" ");
+  }
+  lcd.setCursor(13, 0);
+  lcd.print(firstRunTime);
+  valLength = intToStringToLength(firstRunTime);
+  if (valLength == 1) {
+    lcd.setCursor(13 + valLength, 0);
+    lcd.print("  ");
+  }
+  else if (valLength == 2) {
+    lcd.setCursor(13 + valLength, 0);
+    lcd.print(" ");
+  }
+  lcd.setCursor(5, 1);
+  lcd.print(secondPercent);
+  valLength = intToStringToLength(secondPercent);
+  if (valLength == 1) {
+    lcd.setCursor(5 + valLength, 1);
+    lcd.print("  ");
+  }
+  else if (valLength == 2) {
+    lcd.setCursor(5 + valLength, 1);
+    lcd.print(" ");
+  }
+  lcd.setCursor(13, 1);
+  lcd.print(secondRunTime);
+  valLength = intToStringToLength(secondRunTime);
+  if (valLength == 1) {
+    lcd.setCursor(13 + valLength, 1);
+    lcd.print("  ");
+  }
+  else if (valLength == 2) {
+    lcd.setCursor(13 + valLength, 1);
+    lcd.print(" ");
   }
 }
 
@@ -259,7 +349,7 @@ void manualRun() {
   }
   ///// Start/Stop-button
   if (startStopButtonState != lastStartStopButtonState) {
-    if (startStopButtonState == 1 && lastStartStopButtonState == 0 && millis() - startStopTime > debounce) {
+    if (startStopButtonState == 1 && lastStartStopButtonState == 0 && millis() - startStopTimeMillis > debounce) {
       if (spinning == 1) {
         spinning = 0;
       }
@@ -272,7 +362,7 @@ void manualRun() {
     }
     writeLCD();
     lastStartStopButtonState = startStopButtonState;
-    startStopTime = millis();
+    startStopTimeMillis = millis();
   }
   ///// Left/Right-switch
   if (leftSwitchState != lastLeftSwitchState) {
@@ -321,7 +411,7 @@ void autoRun() {
   */
   ///// Start/Stop-button
   if (startStopButtonState != lastStartStopButtonState) {
-    if (startStopButtonState == 1 && lastStartStopButtonState == 0 && millis() - startStopTime > debounce) {
+    if (startStopButtonState == 1 && lastStartStopButtonState == 0 && millis() - startStopTimeMillis > debounce) {
       if (spinning == 1) {
         spinning = 0;
       }
@@ -331,7 +421,7 @@ void autoRun() {
     }
     writeLCD();
     lastStartStopButtonState = startStopButtonState;
-    startStopTime = millis();
+    startStopTimeMillis = millis();
   }
 
   ////////////////////////////////////////// Auto sequences ///////////////////////////////////////////
@@ -382,6 +472,52 @@ void autoRun() {
     displayTime = 0;
   }
   writeLCD();
+}
+
+void programSequence1() {
+  blinkColon();
+}
+
+void programSequence2() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Program mode 2");
+}
+
+void blinkColon() {
+  if (millis() - blinkColonMillis >= 1000) {
+    if (blinkChar == ":") {
+      blinkChar = "=";
+    }
+    else {
+      blinkChar = ":";
+    }
+    switch (programModeValue) {
+      case 1:
+        lcd.setCursor(4, 0);
+        lcd.print(blinkChar);
+        break;
+      case 2:
+        lcd.setCursor(12, 0);
+        lcd.print(blinkChar);
+        break;
+      case 3:
+        lcd.setCursor(4, 1);
+        lcd.print(blinkChar);
+        break;
+      case 4:
+        lcd.setCursor(12, 1);
+        lcd.print(blinkChar);
+        break;
+    }
+    blinkColonMillis = millis();
+  }
+}
+
+int intToStringToLength(int val) {
+  valString = String(val);
+  valLength = valString.length();
+  return valLength;
 }
 
 void powerControl() {
